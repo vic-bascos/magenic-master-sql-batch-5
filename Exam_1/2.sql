61de7715e0d8d156166d96d07daaedab88061588
vic-bascos <vicb@magenic.com>
2021-09-12 18:52:53 +0800

    Update 2.sql

--- Exam_1/2.sql
+++ Exam_1/2.sql
@@ -1,18 +1,18 @@
 SELECT * 
-INTO Product_20210912
+INTO Product_20210913
 FROM dbo.Product 
 WHERE ModelYear <> 2016
 
 
-UPDATE Product_20210912
+UPDATE Product_20210913
 SET ListPrice = ListPrice * 1.2
-From Product_20210912 Product
+From Product_20210913 Product
 INNER JOIN Brand ON Product.BrandID = Brand.BrandID
 WHERE Brand.BrandName = 'Heller' OR Brand.BrandName = 'Sun Bicycles'
 
 
-UPDATE Product_20210912
+UPDATE Product_20210913
 SET ListPrice = ListPrice * 1.1
-FROM Product_20210912 Product
+FROM Product_20210913 Product
 INNER JOIN Brand ON Product.BrandID = Brand.BrandID
 WHERE Brand.BrandName <> 'Heller' AND Brand.BrandName <> 'Sun Bicycles'
